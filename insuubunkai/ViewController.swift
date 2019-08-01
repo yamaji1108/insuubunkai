43670bbcc48c163c50c185385bc403081f5b5021
yamaji1108 <yamaji1108@gmail.com>
2019-08-01 13:54:33 +0900

    Initial Commit

---
+++ insuubunkai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  insuubunkai
+//
+//  Created by 山田拓也 on 2019/08/01.
+//  Copyright © 2019 山田拓也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
